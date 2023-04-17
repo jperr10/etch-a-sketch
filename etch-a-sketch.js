38b31b8e5067b0e4d7c00fa425bd9f61e9aea324
Jeff Perrin <jeffreyperrin10@gmail.com>
2023-04-17 11:38:43 -0400

    Allow user to cancel input

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,10 +1,13 @@
-const grid = document.querySelector('#grid-container');
+const grid = document.querySelector('#grid');
 let boxes = [];
 
 
 function buttonClicked() {
     length = prompt("Enter an integer between 2 & 100", "Number");
-    while (length < 2 || length > 100) { //check if integer 
+    if (length === null) {
+        return;
+    }
+    while (length < 2 || length > 100 || length % 1 !== 0) { 
         length = prompt("Make sure it is an integer between 2 & 100", "Number");
     };
     if (boxes.length > 0) {
@@ -18,10 +21,7 @@ function buttonClicked() {
         square.addEventListener('mousemove', () => {
             square.style.backgroundColor = 'black';
         });
-    });
-    
-    //changeBoxColor(square);
-    
+    });    
 }   
 
 function clearGrid(grid) {
@@ -43,7 +43,7 @@ function createGrid(boxes) {
         const newDiv = document.createElement('div');
         newDiv.id = `${boxes[box]}`;
         newDiv.className = 'boxClear square';
-        newDiv.textContent = boxes[box]; //Eventually clear
+
         const flexBasis = 90 / length;
         newDiv.style.flex = `1 0 ${flexBasis}%`;
         grid.appendChild(newDiv);
